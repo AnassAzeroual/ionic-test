fac697d066bc369b92e6435ff479194261f68aca
Anass Azeroual <anass.azeroual@flybotix.com>
2024-02-04 01:36:57 +0100

    add component

---
+++ src/app/shared/components/custom-input/custom-input.component.ts
@@ -0,0 +1,52 @@
+/* eslint-disable @angular-eslint/no-input-rename */
+import { Component, Input, OnDestroy, forwardRef } from '@angular/core';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { IonHeader, IonIcon, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-custom-input',
+  templateUrl: './custom-input.component.html',
+  styleUrls: ['./custom-input.component.scss'],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonIcon,FormsModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => CustomInputComponent),
+      multi: true
+    }
+  ]
+})
+export class CustomInputComponent implements ControlValueAccessor, OnDestroy {
+  @Input('name') name!: string;
+  @Input('placeholder') placeholder!: string;
+
+
+  isDisabled = false;
+  model: any;
+
+  private onTouch = () => { };
+  private onChange = (val: any) => { };
+
+
+  inputChanged(e: any) {
+    this.onChange(e);
+    this.onTouch();
+  }
+
+  writeValue(val: any): void {
+    this.model = val;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+  ngOnDestroy(): void {
+    this.model = null;
+  }
+}
--- src/app/welcome/welcome.page.ts
+++ src/app/welcome/welcome.page.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { HeaderComponent } from '../shared/components/header/header.component';
 import { DividerComponent } from '../shared/components/divider/divider.component';
+import { CustomInputComponent } from '../shared/components/custom-input/custom-input.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: 'welcome.page.html',
   styleUrls: ['welcome.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent,HeaderComponent,DividerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent,HeaderComponent,DividerComponent,CustomInputComponent,ReactiveFormsModule],
 })
 export class WelcomePage {
-  constructor() {}
+  form!: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.form = fb.group({
+      fullName: ['Anass Azeroual', []],
+      intention: ['Dev', []],
+      dream: ['family', []],
+    });
+  }
 }
