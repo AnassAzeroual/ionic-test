947f9ede776187e39b0775b8f7c363fda1c59af6
Anass Azeroual <anass.azeroual@flybotix.com>
2024-02-05 23:31:56 +0100

    styles

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import { IonHeader, IonIcon, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { IonContent, IonHeader, IonIcon, IonMenu, IonTitle, IonToolbar, MenuController } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   standalone:true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonIcon],
+  imports: [IonicModule],
 })
 export class HeaderComponent implements OnInit {
-
+  public actionSheetButtons = [
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'Share',
+      data: {
+        action: 'share',
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
   constructor() { }
   ngOnInit(): void {console.log('');
   }
